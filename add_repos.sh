b99e0c046fc7b2840aabddbf3688430f702b0442
seanso <47143083+seanso@users.noreply.github.com>
2023-12-05 15:51:09 +0200

    Added all repositories and script

---
+++ add_repos.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# YAML file path
+yaml_file="test/values.yaml"
+
+# Prompt for ArgoCD user
+read -p "Enter ArgoCD user: " argocd_username
+echo
+
+# Prompt for ArgoCD password
+read -s -p "Enter ArgoCD password: " argocd_password
+echo
+
+# Read repositories from YAML file
+repos=($(yq eval '.services[].repository' "$yaml_file" | sed 's/^"\(.*\)"$/\1/'))
+
+# Loop through each repository and add it to ArgoCD
+for repo_url in "${repos[@]}"; do
+  # Extract repository name from the URL
+  repo_name=$(basename "$repo_url" .git)
+
+  # Run argocd repo add command
+  argocd repo add --name "$repo_name" "$repo_url" --username "$argocd_username" --password "$argocd_password"
+
+  # Print a separator for better readability
+  echo "----------------------------------------"
+done
+
